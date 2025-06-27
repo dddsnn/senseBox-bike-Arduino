67c57d7845441f5347aa26e13889e1a48a1e4b9b
Marc Lehmann <marc@navisense.de>
2025-06-27 06:04:41 +0200

    Increase acceleration filter bandwidth

--- senseBox-bike-atrai-v2/src/sensors/AccelerationSensor/AccelerationSensor.cpp
+++ senseBox-bike-atrai-v2/src/sensors/AccelerationSensor/AccelerationSensor.cpp
@@ -26,7 +26,7 @@ void AccelerationSensor::initSensor()
     Serial.println("MPU6050 Found!");
     mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
     mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
     delay(100);
   };
 
